809c1f3c4ced17c1e5b7eb364736ad615e687d01
tieuphu1998 <nguyenthekien0303@gmail.com>
2016-11-28 18:25:45 +0700

    Update hangman.c
    Updated

--- hangman.c
+++ hangman.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
+//#include<stdio.h>
+char b[10],tu[10]; // tu[10] la 1 mang ki tu, la tu random duoc chon. b[10] la mang ki tu ***
+void in_ra_man_hinh(int n)
+{
+	int i;
+	for(i=1;i<=n;i++) b[i] = 42;
+	for(i=1;i<=n;i++) printf("%c ",b[i]); 
+}
+
+void hien_tu(char &nhap, int n) // n = so ki tu nhap tu dau
+{
+	int dem1,i;
+	dem1 = 0;
+	for(i=1;i<=n;i++) if(nhap==tu[i]) 
+	{
+	b[i] = tu[i];
+	dem1++;
+	}
+	if(dem1) for(i=1;i<=n;i++) printf("%c ",b[i]);
+}
+void cong_chao()
+{
+	printf("CHAO MUNG DEN VOI TRO CHOI\n");
+	printf("\n****   ****      ***      ****   ****  **********         **************************    ");
+	printf("\n*  *   *  *     *   *     *   *  *  *  *        *         *                        *    ");
+	printf("\n*  *****  *    *  *  *    *     **  *  *   ******         *      *******************    ");
+	printf("\n*         *   *       *   *         *  *   *    *****     *     *               *       ");
+	printf("\n*  *****  *  *  *****  *  *  **     *  *   ******  *      *    *                *       ");
+	printf("\n*  *   *  * *  *     *  * *  *  *   *  *           *      *    *               ***      ");
+	printf("\n****   **** ***       *** ****   ****  *************      *    *              *   *     ");
+	printf("\n                                                          *    *               ***      ");
+	printf("\n****     ****      ***      ****   ****                   *    *                *       ");
+	printf("\n*   *   *   *     *   *     *   *  *  *                   *    *                *       ");
+	printf("\n*    * *    *    *  *  *    *     **  *                   *    *              * * *     ");
+	printf("\n*           *   *       *   *         *        ************    ************     *       ");
+	printf("\n*   *   *   *  *  *****  *  *  **     *        *                          *     *       ");
+	printf("\n*    * *    * *  *     *  * *  *  *   *        *                          *    * *      ");
+	printf("\n****     **** ***       *** ****   ****        ****************************             ");
+}
 int main() {
   cong_chao();
   int net_ve;
@@ -23,3 +62,4 @@ int main() {
   printf("Thua\n");
   return 0;
 }
+
